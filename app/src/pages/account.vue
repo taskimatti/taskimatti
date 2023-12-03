81ff2c52d58818298b833f57ec7bc09641afacd8
Jeb <commit@jeb4.dev>
2023-12-03 08:09:12 +0200

    Get user data

--- app/src/pages/account.vue
+++ app/src/pages/account.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import { inject } from "vue";
 
-const user = inject("user");
 </script>
 
 <template>
-  <div>
-    <div class="flex justify-center items-center flex-col">
-      <div>
-        <table class="table-auto">
-          <tr>
-            <th>Nimi</th>
-            <th>Pisteet</th>
-            <th>Admin</th>
-          </tr>
-          <tr>
-            <td>{{ user.name }}</td>
-            <td>{{ user.points }}</td>
-            <td>{{ user.isAdmin }}</td>
-          </tr>
-        </table>
-      </div>
-    </div>
+  <div class="container">
+    <Account :uuid="$route.params.id" />
   </div>
 </template>
 
