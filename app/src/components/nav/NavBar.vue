11b6de8259ce82b4e74978edac7400d0886c7360
Jeb <commit@jeb4.dev>
2023-12-04 20:32:28 +0200

    server side

--- app/src/components/nav/NavBar.vue
+++ app/src/components/nav/NavBar.vue
@@ -13,6 +13,7 @@ import {
   SparklesIcon as SparklesIconOutline,
 } from "@heroicons/vue/24/outline";
 import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 
 const { $directus, $readItems } = useNuxtApp();
 
@@ -29,9 +30,12 @@ const { data: role } = await useAsyncData(() => {
 });
 
 let project = ref("");
+const route = useRoute();
+project.value = route.params.project.toString();
 
 // client only
 if (process.client) {
+  localStorage.setItem("project-uuid", project.value); // get the uuid from local storage
   project.value = localStorage.getItem("project-uuid");
 }
 </script>
@@ -43,8 +47,14 @@ if (process.client) {
     :style="'background: ' + Organisation.color_scheme + ';'"
   >
     <div class="flex items-center justify-around gap-0 m-auto">
-      <nuxt-link :to="'/' + project" class="p-4 h-full w-full flex justify-center items-center flex-col">
-        <CheckBadgeIconSolid v-if="$route.path.split('/')[0] === '/'" class="h-6 w-6 text-white" />
+      <nuxt-link
+        :to="'/' + project"
+        class="p-4 h-full w-full flex justify-center items-center flex-col"
+      >
+        <CheckBadgeIconSolid
+          v-if="$route.path.split('/')[2] == null"
+          class="h-6 w-6 text-red-600"
+        />
         <CheckBadgeIconOutline v-else class="h-6 w-6 text-white" />
         <p class="text-white text-sm">Tasks</p>
       </nuxt-link>
@@ -53,12 +63,21 @@ if (process.client) {
         :to="'/' + project + '/scoreboard'"
         class="p-4 h-full w-full flex justify-center items-center flex-col"
       >
-        <ChartBarIconSolid v-if="$route.path === '/scoreboard'" class="h-6 w-6 text-white" />
+        <ChartBarIconSolid
+          v-if="$route.path === '/scoreboard'"
+          class="h-6 w-6 text-white"
+        />
         <ChartBarIconOutline v-else class="h-6 w-6 text-white" />
         <p class="text-white text-sm">Scoreboard</p>
       </nuxt-link>
-      <nuxt-link :to="'/' + project + '/account'" class="p-4 h-full w-full flex justify-center items-center flex-col">
-        <UserIconSolid v-if="$route.path === '/account'" class="h-6 w-6 text-white" />
+      <nuxt-link
+        :to="'/' + project + '/account'"
+        class="p-4 h-full w-full flex justify-center items-center flex-col"
+      >
+        <UserIconSolid
+          v-if="$route.path.split('/')[2] === '/account'"
+          class="h-6 w-6 text-white"
+        />
         <UserIconOutline v-else class="h-6 w-6 text-white" />
 
         <p class="text-white text-sm">Account</p>
@@ -68,7 +87,10 @@ if (process.client) {
         :to="'/' + project + '/admin'"
         class="p-4 h-full w-full flex justify-center items-center flex-col"
       >
-        <SparklesIconSolid v-if="'/' + $route.path.split('/')[2] === 'admin'" class="h-6 w-6 text-white" />
+        <SparklesIconSolid
+          v-if="'/' + $route.path.split('/')[2] === 'admin'"
+          class="h-6 w-6 text-white"
+        />
         <SparklesIconOutline v-else class="h-6 w-6 text-white" />
 
         <p class="text-white text-sm">Admin</p>
--- app/src/pages/[project]/admin.vue
+++ app/src/pages/[project]/admin.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import { inject } from "vue";
 
-const user = inject("user");
-const admin = user.isAdmin;
 </script>
 
 <template>
-  <div v-if="admin">
+  <div v-if="false">
     <h1 class="text-center">Olet Admin</h1>
   </div>
 </template>
