fd9d7644140e836ccc86346928a0494bb983af77
Jeb <commit@jeb4.dev>
2023-12-05 06:44:11 +0200

    get correct units

--- app/src/pages/[project]/index.vue
+++ app/src/pages/[project]/index.vue
@@ -3,17 +3,19 @@ import { ref, watch } from "vue";
 import { onBeforeRouteUpdate } from "vue-router";
 import { useRoute } from "vue-router";
 import { navigateTo } from "nuxt/app";
+import { useProject } from "../../composables/states";
 
 const { $directus, $readItems } = useNuxtApp();
 
 const route = ref(useRoute());
 const uuid = ref(route.value?.params?.project?.toString());
 const Tasks = ref([]);
-const Organisation = ref({});
+const project = ref({});
 
 const updatePage = async () => {
   const _uuid = ref(route.value?.params?.project?.toString());
   uuid.value = _uuid.value;
+  project.value = useProject();
 
   if (uuid.value?.toString().length === 36) {
     Tasks.value = await $directus.request(
@@ -25,14 +27,10 @@ const updatePage = async () => {
         },
       }),
     );
-
-    Organisation.value = await $directus.request($readItems("Organisation"));
   } else {
     navigateTo("/");
   }
 };
-
-await updatePage();
 
 onBeforeRouteUpdate(async (to, from) => {
   if (to.path !== from.path) {
@@ -40,43 +38,14 @@ onBeforeRouteUpdate(async (to, from) => {
   }
 });
 
-watch(
-  () => route.value,
-  async () => {
-    await updatePage();
-  },
-);
-
-watch(
-  () => uuid.value,
-  async (newUuid) => {
-    uuid.value = newUuid;
-    await updatePage();
-  },
-);
-
-watch(
-  () => Tasks.value,
-  async (newTasks) => {
-    Tasks.value = newTasks;
-    await updatePage();
-  },
-);
-
-watch(
-  () => Organisation.value,
-  async (newOrganisation) => {
-    Organisation.value = newOrganisation;
-    await updatePage();
-  },
-);
+updatePage();
 </script>
 
 <template>
   <div>
     <ol>
       <li v-for="task in Tasks" class="p-2">
-        <Task :task="task" :unit="Organisation.units" />
+        <Task :task="task" :unit="project.value?.units" />
       </li>
     </ol>
   </div>
