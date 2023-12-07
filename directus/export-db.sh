96b34469c4427f7fab6fa4bbb608bd82d9780835
Jeb <commit@jeb4.dev>
2023-12-07 02:27:21 +0200

    fuksipassi -> taskimatti

--- directus/export-db.sh
+++ directus/export-db.sh
@@ -2,9 +2,9 @@
 # Description: Export database from docker container to host machine
 
 echo "Exporting database from docker container to host machine..."
-docker exec Fuksipassi-PostgreSQL bash -c "pg_dump -U default -d directus_prod > /home/backup.sql"
+docker exec taskimatti-PostgreSQL bash -c "pg_dump -U default -d directus_prod > /home/backup.sql"
 
 echo "Copying database from docker container to host machine..."
-docker cp Fuksipassi-PostgreSQL:/home/backup.sql data/backup.sql
+docker cp taskimatti-PostgreSQL:/home/backup.sql data/backup.sql
 
 echo "Done!"
