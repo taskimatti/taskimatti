63fb65a48fee4aeeb1d408a555cf3ed723bcd1c5
miikatu <miikatti@gmail.com>
2024-01-27 20:01:50 +0200

    Fixed missing element

--- app/src/components/Account.vue
+++ app/src/components/Account.vue
@@ -42,6 +42,7 @@ const updateUserName =async () => {
     <NuxtImg :src="image.endsWith('null') ? '/images/avatar.jpg' : image" placeholder="/images/avatar.jpg" alt="Avatar"
       width="100" height="100" class="h-32 w-32 object-cover rounded-full border-2 border-gray-600" />
     <h2 class="text-2xl text-white mt-4">{{ user.first_name }}</h2>
+  </div>
   <div class="bg-gray-800 p-6 rounded-lg shadow-md w-full">
     <NuxtImg
       :src="image.endsWith('null') ? '/images/avatar.jpg' : image"
